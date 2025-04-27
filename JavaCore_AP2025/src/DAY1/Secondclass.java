2cf9597f0aaebbfe41bc43dd68e01c567d05fa4b
Maaz Choudhary <maazchoudhary123456@gmail.com>
2025-04-27 14:43:22 +0530

    Second commit

---
+++ JavaCore_AP2025/src/DAY1/Secondclass.java
@@ -0,0 +1,11 @@
+package DAY1;
+
+public class Secondclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is just for practise ");
+
+	}
+
+}
